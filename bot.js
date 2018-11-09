7162f84db14dcb4dbb0cc765c2645f2f786c7a3d
Fuli33 <44379088+Fuli33@users.noreply.github.com>
2018-11-10 02:35:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -317,3 +317,27 @@ function play(guild, song) {
 
 
 client.login(process.env.BOT_TOKEN);///لاتغير شي
+const adminprefix = "$";
+const devs = ['271739675759083520','271739675759083520'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'بلاي')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'نيم')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'افتار')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'ستريم')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوق دايموند كودز
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق دايموند كودز
+}
+});
